907145145b6612ac198d87b22d266eb4395975a3
Trenton Tong <57164041+Trenton140@users.noreply.github.com>
2024-04-03 17:07:28 -0400

    Setup React app for deployment

---
+++ trentontong/src/App.js
@@ -0,0 +1,13 @@
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Trenton Tong</h1>
+        <p>Coming Soon</p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
+
